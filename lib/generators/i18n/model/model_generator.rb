36784b56e61a4b54d66870de66a8d2e638e5c589
kissy2go <kissy2go@gmail.com>
2015-12-07 13:57:56 +0900

    Implement model generator

--- lib/generators/i18n/model/model_generator.rb
+++ lib/generators/i18n/model/model_generator.rb
@@ -7,10 +7,46 @@ module I18n
 
       # @!attribute
       #   @return [String] name Model name
-      argument :name, type: :string
+      argument :name, type: :string, banner: 'model_name'
 
       # Create I18n locale files.
       def create_locale_files
+        @i18n_scope = model.i18n_scope.to_s
+        @i18n_key   = model.model_name.i18n_key.to_s
+        @columns    = model.column_names
+
+        locales.each do |locale|
+          @locale = locale
+          locale_file = File.join(
+            'config/locales/models', model_name.underscore, "#{@locale}.yml"
+          )
+          template 'locale.yml', locale_file
+        end
+      rescue NameError
+        puts "#{model_name} is undefined."
+      end
+
+      private
+
+      # Return model name
+      #
+      # @return [String] Model name
+      def model_name
+        name.classify
+      end
+
+      # Return model class.
+      #
+      # @return [Class] Model class
+      def model
+        model_name.constantize
+      end
+
+      # Return generating locale names
+      #
+      # @return [Array<Symbol|String>] Locale names
+      def locales
+        I18n.available_locales
       end
     end
   end
