26897a92bd6d3c75cc528fa57d22ca543619490f
kissy2go <kissy2go@gmail.com>
2015-12-07 11:56:52 +0900

    Use rspec-given and rewrite specs

--- spec/i18n_generator_spec.rb
+++ spec/i18n_generator_spec.rb
@@ -1,11 +1,5 @@
 require 'spec_helper'
 
 describe I18nGenerator do
-  it 'has a version number' do
-    expect(I18nGenerator::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
+  Then { I18nGenerator::VERSION != nil }
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,3 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'rspec/given'
 require 'i18n_generator'
